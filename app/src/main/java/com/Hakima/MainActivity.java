c268e8891f35e3ac96fe68b63e08d5865691f506
Mohcine Belarrem <mohcine@weekcal.com>
2019-02-05 17:41:59 +0100

    Simple Hello App

--- app/src/main/java/com/Hakima/MainActivity.java
+++ app/src/main/java/com/Hakima/MainActivity.java
@@ -1,15 +1,26 @@
 package com.Hakima;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt;
+    Button botton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        txt = (EditText) findViewById(R.id.textEdit);
+        botton = (Button) findViewById(R.id.botton);
+
+
+
+
+
+        botton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String s = new String();
+
+                if ( txt.getText().length() == 0 ) {
+                    s = "Ktbi smitek";
+                } else {
+                    s = "Ahlan " + txt.getText() + " Labass?";
+                }
+
+            txt.onEditorAction(EditorInfo.IME_ACTION_DONE);
+
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Salam");
+                alertDialog.setMessage(s);
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
